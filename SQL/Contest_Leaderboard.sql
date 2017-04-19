d304a819299cdebb76c309dc66b07c2fd5f3f129
WayToTheTop <yauheni.koran@gmail.com>
2017-04-18 18:34:37 -0700

    Create Contest_Leaderboard.sql

---
+++ SQL/Contest_Leaderboard.sql
@@ -0,0 +1,6 @@
+--https://www.hackerrank.com/challenges/contest-leaderboard/submissions/database/42632001
+SELECT h.hacker_id, h.name, SUM(t.max_score) AS total
+FROM (SELECT hacker_id, challenge_id, MAX(score) AS max_score FROM submissions GROUP BY 1,2) as t, hackers AS h 
+WHERE h.hacker_id = t.hacker_id
+GROUP BY h.hacker_id, h.name HAVING total>0
+ORDER BY total DESC, h.hacker_id
