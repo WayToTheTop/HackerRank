1ca9b0fdb4292e448615f80a690008ba09db60e8
WayToTheTop <yauheni.koran@gmail.com>
2017-04-18 18:33:40 -0700

    Create Projects.sql

---
+++ SQL/Projects.sql
@@ -0,0 +1,7 @@
+--https://www.hackerrank.com/domains/sql/advanced-join
+SELECT t1.start_date, MIN(t2.end_date) FROM
+(SELECT start_date FROM projects WHERE start_date NOT IN (SELECT end_date FROM projects)) t1,
+(SELECT end_date FROM projects WHERE end_date NOT IN (SELECT start_date FROM projects)) t2
+WHERE t1.start_date < t2.end_date
+GROUP BY 1
+ORDER BY (MIN(t2.end_date) - t1.start_date),1
